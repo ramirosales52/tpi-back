228d1b9d7cfdc954ec5e3af3497961f2696d33f0
Ramiro <ramirosales52@gmail.com>
2022-11-23 12:08:53 -0300

    update back

--- models/denuncia_model.js
+++ models/denuncia_model.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose')
 
 const denunciasSchema = new mongoose.Schema({
+    denunciaanonima: {
+        type: Boolean,
+        require: true,
+        default: false
+    },
     nombre: {
         type: String,
         require: true
@@ -9,12 +14,31 @@ const denunciasSchema = new mongoose.Schema({
         type: String,
         require: true
     },
+    tipodni: {
+        type: String,
+        require: true
+    },
+    dni: {
+        type: Number,
+        require: true
+    },
+    telmovil: {
+        type: Number
+    },
+    telfijo: {
+        type: Number
+    },
     localidad: {
         type: String,
         require: true
     },
     email: {
         type: String,
+        require: true
+    },
+    denunciado: {
+        type: Boolean,
+        default: false,
         require: true
     },
     nombredenunciado: {
@@ -29,10 +53,15 @@ const denunciasSchema = new mongoose.Schema({
         type: String,
         require: true
     },
-    motivo: {
+    descripcion: {
         type: String,
         require: true
+    },
+    fecha: {
+        type: Date,
+        default: Date.now
     }
+    
 })
 
 module.exports = mongoose.model('Denuncia', denunciasSchema)
--- routes/denuncias.routes.js
+++ routes/denuncias.routes.js
@@ -48,6 +48,18 @@ route.get('/', (req, res) => {
     })
 })
 
+route.get('/:email/:id', (req, res) => {
+    let result = getDenunciaById(req.params.id)
+    result.then(value => {
+        res.json({
+            value: value
+        })
+    }) .catch(err =>{
+        res.status(400).json({
+            error: err
+        })                                                                  
+    })
+})
 
 route.get('/:email', (req, res) => {
     let result = getDenunciasByEmail(req.params.email)
@@ -64,16 +76,25 @@ route.get('/:email', (req, res) => {
 
 
 
+
 async function crearDenuncia(body){
     let denuncia = new Denuncia({
+        denunciaanonima     : body.denunciaanonima,
         nombre              : body.nombre,
         apellido            : body.apellido,
+        tipodni             : body.tipodni,
+        dni                 : body.dni,
+        telmovil            : body.telmovil,
+        telfijo             : body.telfijo,
         localidad           : body.localidad,
         email               : body.email,
+        denunciado          : body.denunciado,
         nombredenunciado    : body.nombredenunciado,
         apellidodenunciado  : body.apellidodenunciado,
+        domiciliodenunciado : body.domiciliodenunciado,
         tipoinfraccion      : body.tipoinfraccion,
-        motivo              : body.motivo
+        descripcion         : body.descripcion,
+        fecha               : body.fecha
     })
 
     return await denuncia.save() 
@@ -90,6 +111,10 @@ async function getDenunciasByEmail(email){
     return denunciasbyemail
 }
 
+async function getDenunciaById(id){
+    let denunciabyid = await Denuncia.findById(id)
+    return denunciabyid
+}
 
 
 module.exports = route;
